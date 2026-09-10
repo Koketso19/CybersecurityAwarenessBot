007bfbda73029bee738525ff23c41e8d08615576
KOKETSO LEGOABE <koketso.lgoabe@apex-rts.co.za>
2026-09-11 00:04:38 +0200

    Add UserProfile class to CybersecurityAwarenessBot
    Introduce a new namespace `CybersecurityAwarenessBot` and add the `UserProfile` class within it. The `UserProfile` class includes an automatic property `Name` of type `string` with `get` and `set` accessors. This addition aligns with the POE requirements to support user profile management.

---
+++ CybersecurityAwarenessBot/CybersecurityAwarenessBot/UserProfile.cs
@@ -0,0 +1,8 @@
+﻿namespace CybersecurityAwarenessBot
+{
+    public class UserProfile
+    {
+        // Automatic property as required by the POE
+        public string Name { get; set; }
+    }
+}
